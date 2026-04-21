61f2469b33d13375fa603689db7d34d810935a10
Maksym Cherpitskyi <prizrakooo72@gmail.com>
2026-04-22 00:30:48 +0300

    Added ability to screenshot browser page

---
+++ CoreLayer/BrowserUtils.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi.BrowsingContext;
+namespace CoreLayer
+{
+    internal static class BrowserUtils
+    {
+        public static class ScreenshotMaker
+        {
+            public static string TakeBrowserScreenshot(ITakesScreenshot driver)
+            {
+                var now = DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-fff");
+                var screenshotPath = Path.Combine(Environment.CurrentDirectory, $"Display_{now}.png");
+                driver.GetScreenshot().SaveAsFile(screenshotPath);
+
+                return screenshotPath;
+            }
+        }
+    }
+}
