a42bff60813dcda9c46b4f18661082340ec0fc9a
Maksym Cherpitskyi <prizrakooo72@gmail.com>
2026-05-21 14:30:22 +0300

    test using default location for ubuntu image

--- CoreLayer/Webdriver/WebDriverFactory.cs
+++ CoreLayer/Webdriver/WebDriverFactory.cs
@@ -22,7 +22,7 @@ namespace CoreLayer.Webdriver
                     var firefoxOptions = new FirefoxOptions();
                     firefoxOptions.AddArgument("--start-maximized");
                     firefoxOptions.AddArgument("--headless");
-                    firefoxOptions.BinaryLocation = "/snap/bin/geckodriver";
+                    firefoxOptions.BinaryLocation = "/usr/local/share/gecko_driver";
                     return new FirefoxDriver(firefoxOptions);
                 default:
                     throw new ArgumentException("Unknown browser type: " + browserType);
