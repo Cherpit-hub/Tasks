90396b4e18f01da405949225404ca13792e72d42
Maksym Cherpitskyi <prizrakooo72@gmail.com>
2026-05-21 14:19:43 +0300

    Change location to geckodriver

--- CoreLayer/Webdriver/WebDriverFactory.cs
+++ CoreLayer/Webdriver/WebDriverFactory.cs
@@ -22,7 +22,7 @@ namespace CoreLayer.Webdriver
                     var firefoxOptions = new FirefoxOptions();
                     firefoxOptions.AddArgument("--start-maximized");
                     firefoxOptions.AddArgument("--headless");
-                    firefoxOptions.BinaryLocation = "/snap/bin/firefox";
+                    firefoxOptions.BinaryLocation = "/snap/bin/geckodriver";
                     return new FirefoxDriver(firefoxOptions);
                 default:
                     throw new ArgumentException("Unknown browser type: " + browserType);
